e09d5f8792195b0a678e494360d3564c24f9d8c6
manel yagoub <douaa.yagoub@ensia.edu.dz>
2023-03-09 14:39:47 +0100

    commit

--- src/Start.java
+++ src/Start.java
@@ -1,9 +1,10 @@
 
 public class Start {
+	//this is me adding some comments
 
 	public static void main(String[] args) {
 		
-		System.out.println("hello world!");
+		System.out.println("hello world!");//prints hello world in console
 	}
 
 }
